08fa4966156f9ac8273f2f9ece5a100d5de7066a
Kamil Malunow <97669214+KamilMalunow@users.noreply.github.com>
2022-12-07 11:50:11 +0100

    Add New Card Section

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const questionText = document.querySelector('.question--textarea');
 const result = document.querySelector('.result');
 
 const tablica = [{
-    question: "pytanie",
-    anwser: "odpowiedz",
+    question: "Pytanie",
+    anwser: "Odpowiedz",
 }];
 let text;
 
@@ -21,8 +21,43 @@ function closeAddQuestion() {
     main.style.display = "none";
 }
 
+function displayQuestions() {
+
+    if (anwserText.value === "" || questionText.value === "") {
+        alert("Cannot Add Empty Values");
+    } else {
+        for (const a of tablica) {
+            text =
+                `
+        <div class="card">
+            <p>${a.question}</p>
+            <button class="toggle--item">Show/Hide Anwser</button>
+            <p class="item">${a.anwser}</p>
+            <div class="card--buttons">
+                <button class="card--edit">Edit</button>
+                <button class="card--delete">Delete</button>
+            </div> 
+        </div>
+        `
+        }
+        result.innerHTML = text;
+        const showHide = document.querySelector('.toggle--item');
+        showHide.addEventListener("click", toggleButton)
+    }
+}
 
 
 
+function toggleButton(){
+    const item = document.querySelector('.item');
+    item.classList.toggle("show")
+}
+
+
+
+
+
+
+saveQuestionBtn.addEventListener("click", displayQuestions);
 closeAddQuestionBtn.addEventListener("click", closeAddQuestion);
 showQuestionBtn.addEventListener("click", showAddQuestion);
